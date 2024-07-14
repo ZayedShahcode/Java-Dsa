35a7c3f1b3b6c56b94c6106497e096930f587103
void_name_zayed <reachzayed@proton.me>
2024-07-14 22:20:58 +0530

    bubble sort

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+import java.util.*;
+
+ public class BubbleSort {
+
+    static void bubbleSort(int[] arr){
+        int n = arr.length;
+        boolean swapped;
+        for(int i=0;i<n;i++){
+            swapped = false;
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,1,6,5,4,8,2};
+        bubbleSort(arr);
+        System.out.println("\n"+Arrays.toString(arr));
+    }
+}
